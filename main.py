9896fc0dcc65ce72461d984f3632548e2011e95b
nikseleznev <nikitaselez.25@gmail.com>
2025-07-09 20:15:06 +0500

    ggg

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ win = pygame.display.set_mode((700,500))
 clock = pygame.time.Clock()
 
 win.fill((0, 220,255))
+class GameSprite(pygame.sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = pygame.transform.scale(pygame.image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+def reset(self):
+    win.blit(self.image, (self.rect.x, self.rect.y))
 
 is_game = True
 while is_game:
